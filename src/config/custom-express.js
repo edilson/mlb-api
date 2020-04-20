7dae6374e8caa9ce985cd83706fea6dda14645ef
SEdilson <edilson.silva00@hotmail.com>
2020-04-20 11:47:26 -0300

    Add CORS

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { errors } = require('celebrate');
+const cors = require('cors');
 
 const routers = require('../routes/routes');
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(routers);
 app.use(errors());
