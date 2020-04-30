fef359f8aa1fc8ffef4a8fe7013e726bb73086e3
SEdilson <edilson.silva00@hotmail.com>
2020-04-29 21:59:03 -0300

    Add validation on Division Series routes

--- src/routes/divisionSeriesRoutes.js
+++ src/routes/divisionSeriesRoutes.js
@@ -7,13 +7,61 @@ const routes = express.Router();
 
 routes
   .route('/division_series')
-  .post(DivisionSeriesController.create)
-  .get(DivisionSeriesController.list);
+  .post(
+    celebrate({
+      [Segments.BODY]: Joi.object().keys({
+        start_date: Joi.date().iso().required(),
+        end_date: Joi.date()
+          .iso()
+          .ruleset.greater(Joi.ref('start_date'))
+          .rule({ message: 'end_date must be greater than start_date' })
+          .required(),
+        champion_id: Joi.string().required(),
+        record: Joi.string().required().min(5),
+      }),
+    }),
+    DivisionSeriesController.create
+  )
+  .get(
+    celebrate({
+      [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+      }),
+    }),
+    DivisionSeriesController.list
+  );
 
 routes
   .route('/division_series/:id')
-  .get(DivisionSeriesController.findById)
-  .put(DivisionSeriesController.update)
-  .delete(DivisionSeriesController.delete);
+  .get(
+    celebrate({
+      [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().integer().required(),
+      }),
+    }),
+    DivisionSeriesController.findById
+  )
+  .put(
+    celebrate({
+      [Segments.BODY]: Joi.object().keys({
+        start_date: Joi.date().iso(),
+        end_date: Joi.date()
+          .iso()
+          .ruleset.greater(Joi.ref('start_date'))
+          .rule({ message: 'end_date must be greater than start_date' }),
+        champion_id: Joi.string(),
+        record: Joi.string().min(5),
+      }),
+    }),
+    DivisionSeriesController.update
+  )
+  .delete(
+    celebrate({
+      [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().integer().required(),
+      }),
+    }),
+    DivisionSeriesController.delete
+  );
 
 module.exports = routes;
